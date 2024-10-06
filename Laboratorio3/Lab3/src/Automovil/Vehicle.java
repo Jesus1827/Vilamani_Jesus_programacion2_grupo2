5d5a6d68fc76203881974241562be27616990043
Jesus1827 <jesusvilamani22@gmail.com>
2024-10-06 00:20:54 -0400

    Vehicle.java

--- Laboratorio3/Lab3/src/Automovil/Vehicle.java
+++ Laboratorio3/Lab3/src/Automovil/Vehicle.java
@@ -4,21 +4,21 @@ public class Vehicle {
     private String brand;
     private String model;
     private double price;
-
+// Constructor
     public Vehicle(String brand, String model, double price) {
         this.brand = brand;
         this.model = model;
         this.price = price;
     }
-
+// Método para arrancar
     public void start() {
         System.out.println(brand + " " + model + " is starting.");
     }
-
+// Método para detener
     public void stop() {
         System.out.println(brand + " " + model + " is stopping.");
     }
-
+// Método para mostar imformacion del vehiculo
     public void displayInfo() {
         System.out.println("Brand: " + brand + ", Model: " + model + ", Price: " + price);
     }
